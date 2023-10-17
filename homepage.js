4e2c2a24ca20f7e80894c5452731b21dd6188763
Matteo Pucci <matteopuccifxgmail.com>
2023-10-17 15:02:03 +0200

    second album

--- homepage.js
+++ homepage.js
@@ -1,5 +1,6 @@
 // RINTRACCIO LA ROW TARGET
 const myRow = document.getElementById("mainRow");
+const secondRow = document.getElementById("secondRow");
 
 fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=hiphop")
   .then((res) => {
@@ -17,7 +18,23 @@ fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=hiphop")
     console.log(err);
   });
 
-// FUNCTION PER GENERARE LE CARDS
+fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=pop")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error("Errore");
+    }
+  })
+  .then((data) => {
+    console.log("Search", data);
+    generateSecondCards(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// FUNCTION PER GENERARE LE CARDS HIPHOP
 const generateCards = (album) => {
   album.data.forEach((el, index) => {
     if (index < 6) {
@@ -40,6 +57,32 @@ const generateCards = (album) => {
     </div>
       `;
       myRow.appendChild(newCol);
+    }
+  });
+};
+// FUNCTION PER GENERARE LE CARDS POP
+const generateSecondCards = (album) => {
+  album.data.forEach((el, index) => {
+    if (index < 6) {
+      const newCol = document.createElement("div");
+      newCol.classList.add("col", "mt-5");
+      newCol.innerHTML = `
+    <div class="card h-100">
+        <img src="${el.album.cover_big}" class="card-img-top" alt="img-${index}">
+        <div class="card-body d-flex flex-column justify-content-between">
+            <div>
+            <h5 class="card-title"><a class="text-decoration-none text-white" href="/albumPage/album.html?albumId=${el.album.id}">${el.album.title}</a></h5>
+            
+            </div>
+            <div>
+            <p class="card-text"> <a class="text-white" href="#">${el.artist.name}</a></p>
+            
+            </div>
+            
+        </div>
+    </div>
+      `;
+      secondRow.appendChild(newCol);
     }
   });
 };
