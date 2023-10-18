13770cc66a3a18bb182f836bb3d1ae9c1d27ae73
Marissa Burca <Marissaburca@gmail.com>
2023-10-18 18:02:44 +0200

    menu in basso fatto; da abbellire

--- js/homepage.js
+++ js/homepage.js
@@ -22,7 +22,7 @@ fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=hiphop")
 
 /* card per screem grande */
 
-/* const generateCards = (album) => {
+const generateCards = (album) => {
   album.data.forEach((el, index) => {
     if (index < 16) {
       const newCol = document.createElement("div");
@@ -45,94 +45,10 @@ fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=hiphop")
       myRow.appendChild(newCol);
     }
   });
-}; */
-
-
-const generateCards = (album) => {
-  album.data.forEach((el, index) => {
-    if(index<16){
-    const newCol = document.createElement("div");
-    newCol.classList.add("col", "my-2", "p-1");
-
-    if (window.innerWidth > 910) {
-      newCol.innerHTML = `
-        <div class="card h-100 border-0 p-2 bg-white-50" style="width: 12rem;">
-          <img src="${el.album.cover_big}" class="card-img-top img-fluid" alt="img-${index}">
-          <div class="card-body d-flex flex-column justify-content-between px-0">
-            <div>
-              <h6 class="card-title"><a class="text-decoration-none text-white" href="/albumPage/album.html?albumId=${el.album.id}">${el.album.title}</a></h6>
-            </div>
-            <div>
-              <p class="card-text"> <a class="text-white" href="#">${el.artist.name}</a></p>
-            </div>
-          </div>
-        </div>
-      `;
-    } else {
-      newCol.innerHTML = `
-      <div class="card border-0 px-4 pt-4 bg-white-50" style="width:18rem;">
-      <div>
-         <img src="${el.album.cover_big}" class="img-fluid w-50" alt="img-${index}">
-       <span class="card-title ps-3"><a class="text-decoration-none text-white" href="/albumPage/album.html?albumId=${el.album.id}">${el.album.title}</a></span>
-      </div>
-      <div class="card-body d-flex flex-column justify-content-between px-0">
-      <div>
-      <button id="heart-player" onclick="selectedHeart()" class="col-2">
-      <i class="bi bi-heart d-flex" id="heart"></i>
-      <i class="bi bi-heart-fill d-none" id="heart-fill"></i>
-      </button>
-      <i class="bi bi-three-dots-vertical"></i>
-      </div>
-      <div>
-      <p class="card-text"> <a class="text-seconday" href="#">${el.data.length}</a></p>
-        </div>
-      </div>
-    </div>
-      `;
-    }
-
-    myRow.appendChild(newCol);}
-  });
 };
 
-function onResize() {
-  if (window.innerWidth > 910) {
-    generateCards(data);
-  } else {
-    myRow.innerHTML = "";
-
-    // Genera il nuovo contenuto
-    generateCards(data);
-  }
-}
-
-// Richiamo la funzione al caricamento della pagina
-onResize();
-
-// Aggiungi un eventlistener per l'evento resize
-window.addEventListener("resize", onResize);
 
 
-
-// RINTRACCIO LA COL TARGET
-const activityCol = document.getElementById("activityCol");
-const contentCol = document.getElementById("section");
-const aside2 = document.getElementById("aside2");
-
-const closeFriendsPage = (e) => {
-  // console.log(activityCol);
-  activityCol.classList.add("d-none");
-  activityCol.classList.remove("new-width2");
-  contentCol.classList.remove("new-width");
-};
-
-const openFriendsPage = (e) => {
-  // console.log(e.target);
-  activityCol.classList.remove("d-none");
-  activityCol.classList.add("new-width2");
-  contentCol.classList.add("new-width");
-  aside2.classList.add("new-width2");
-};
 
 /* LOGICA PLAYER */
 
@@ -289,5 +205,4 @@ function mutedAudio() {
   }
 }
 
-/*  FUNZIONE PER MEDIA SCREEN */
 
